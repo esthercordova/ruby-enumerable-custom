8b2fcda899c90aeba4a541762805e0d5e4366645
esthercordova <estherpiacordova@gmail.com>
2016-06-10 13:46:19 -0400

    yeah

--- lib/card.rb
+++ lib/card.rb
@@ -1,7 +1,9 @@
 # A simple representation of a playing card.
 class Card
+  include Comparable
   SUITS = %w(C D H S).freeze
   RANKS = [(2..10).to_a, %w(J Q K A)].flatten
+
 
   attr_reader :suit, :rank
 
@@ -15,7 +17,19 @@ class Card
     @rank = rank
   end
 
+  def <=>(other)
+  #compare by suit
+   suit_comparison = SUITS.index(suit) <=> SUITS.index(other.suit)
+   rank_comparison = RANKS.index(rank) <=> RANKS.index(other.rank)
+   if suit_comparison == 0
+     rank_comparison
+  else
+    suit_comparison
+  end
+ end
+
+
   def to_s
-    "#{rank}:#{suit}"
+    "#{rank} of #{suit}"
   end
 end
--- lib/stepped_range.rb
+++ lib/stepped_range.rb
@@ -1,3 +1,17 @@
 #
 class SteppedRange
+
+  include Enumerable
+  def each
+    current = start
+    while current <= stop
+      yield current += step
+  end
+
+  attr_reader :start, :stop, :step
+  def initialize(start, stop, step = 1)
+    @start = start
+    @stop  = stop
+    @step = step
+  end
 end
